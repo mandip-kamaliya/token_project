ec04c021096cb4a17d56a29e745aedf870891901
mandip kamaliya <mandipkamaliya7@gmail.com>
2025-10-20 17:56:02 +0530

    added mint function

--- src/RobustToken.sol
+++ src/RobustToken.sol
@@ -1,5 +1,6 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.23;
+
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Pausable.sol";
@@ -7,25 +8,18 @@ import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Capped.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Votes.sol";
 import "@openzeppelin/contracts/access/AccessControl.sol";
 
-contract RobustToken  is 
-        ERC20,
-        ERCBurnable,
-        ERC20Pausable,
-        ERC20Capped,
-        ERC20Votes,
-        AccessControl
-{
+contract RobustToken is ERC20, ERC20Burnable, ERC20Pausable, ERC20Capped, ERC20Votes, AccessControl {
     bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
     bytes32 public constant PAUSER_ROLE = keccak256("PAUSER_ROLE");
 
-    constructor() ERC20(ROBUST_TOKEN,"RBT") ERCCapped(1_000_000_000 * (10 * decimals())){
+    constructor() ERC20(ROBUST_TOKEN, "RBT") ERCCapped(1_000_000_000 * (10 * decimals())) {
         _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
         _grantRole(MINTER_ROLE, msg.sender);
         _grantRole(PAUSER_ROLE, msg.sender);
 
-        uint256 initialSupply = 100_000_000 * (10**decimals());
+        uint256 initialSupply = 100_000_000 * (10 ** decimals());
         _mint(msg.sender, initialSupply);
-    } 
+    }
 
     function pause() public onlyRole(PAUSER_ROLE) {
         _pause();
@@ -33,5 +27,9 @@ contract RobustToken  is
 
     function unpause() public onlyRole(PAUSER_ROLE) {
         _unpause();
+    }
+
+    function mint(address to, uint256 amount) public onlyRole(MINTER_ROLE) {
+        _mint(to, amount);
     }
 }
