cf2d8bde022dacca13a01b379fb4f807cd13ee73
mandip kamaliya <mandipkamaliya7@gmail.com>
2025-10-20 17:49:33 +0530

    remove ERC20Snapshot and added ERC20Votes

--- src/RobustToken.sol
+++ src/RobustToken.sol
@@ -4,7 +4,7 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Pausable.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Capped.sol";
-import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Snapshot.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Votes.sol";
 import "@openzeppelin/contracts/access/AccessControl.sol";
 
 contract RobustToken  is 
@@ -12,8 +12,15 @@ contract RobustToken  is
         ERCBurnable,
         ERC20Pausable,
         ERC20Capped,
-        ERC20Snapshot,
+        ERC20Votes,
         AccessControl
 {
+    bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
+    bytes32 public constant PAUSER_ROLE = keccak256("PAUSER_ROLE");
 
+    constructor() ERC20(ROBUST_TOKEN,"RBT") ERCCapped(1_000_000_000 (10 * decimals())){
+        _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
+        _grantRole(MINTER_ROLE, msg.sender);
+        _grantRole(PAUSER_ROLE, msg.sender);
+    } 
 }
