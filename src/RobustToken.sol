a16461bc88b52832ab26971da3670aed09b124e5
mandip kamaliya <mandipkamaliya7@gmail.com>
2025-10-20 18:07:35 +0530

    added update function

--- src/RobustToken.sol
+++ src/RobustToken.sol
@@ -6,13 +6,18 @@ import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Pausable.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Capped.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Votes.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
 import "@openzeppelin/contracts/access/AccessControl.sol";
 
 contract RobustToken is ERC20, ERC20Burnable, ERC20Pausable, ERC20Capped, ERC20Votes, AccessControl {
     bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
     bytes32 public constant PAUSER_ROLE = keccak256("PAUSER_ROLE");
 
-    constructor() ERC20(ROBUST_TOKEN, "RBT") ERCCapped(1_000_000_000 * (10 * decimals())) {
+    constructor()
+        ERC20("RobustToken", "RBT")
+        ERC20Capped(1_000_000_000 * (10 * decimals()))
+        EIP712("RobustToken", "1")
+    {
         _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
         _grantRole(MINTER_ROLE, msg.sender);
         _grantRole(PAUSER_ROLE, msg.sender);
@@ -31,5 +36,12 @@ contract RobustToken is ERC20, ERC20Burnable, ERC20Pausable, ERC20Capped, ERC20V
 
     function mint(address to, uint256 amount) public onlyRole(MINTER_ROLE) {
         _mint(to, amount);
+    }
+
+    function _update(address from, address to, uint256 value)
+        internal
+        override(ERC20, ERC20Pausable, ERC20Capped, ERC20Votes)
+    {
+        super._update(from, to, value);
     }
 }
